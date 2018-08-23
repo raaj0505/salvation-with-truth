e2ddd26953a6c666e4d6d732a0e1f598877dad88
raaj0505 <raaj0505@gmail.com>
2018-08-23 19:37:14 +0530

    added index.php for deployment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -6,6 +6,14 @@ export const environment = {
   production: false,
   test: true,
   i18nPrefix: '',
+  firebase: {
+    apiKey: 'AIzaSyBRIkLBv6A4eqiDiu6aepPo_O1BdDO-2Lc',
+    authDomain: 'salvationwithtruth.firebaseapp.com',
+    databaseURL: 'https://salvationwithtruth.firebaseio.com',
+    projectId: 'salvationwithtruth',
+    storageBucket: '',
+    messagingSenderId: '880870946140'
+  },
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
@@ -18,5 +26,7 @@ export const environment = {
       packageJson.dependencies['@fortawesome/fontawesome-free-webfonts'],
     angularCli: packageJson.devDependencies['@angular/cli'],
     typescript: packageJson.devDependencies['typescript']
-  }
+  },
+  numberOfBits: 32,
+  characters: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 };
